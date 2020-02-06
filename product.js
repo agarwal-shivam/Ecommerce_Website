4a122d3becc0f4d9c8e483fa5797f6c275d1a88b
agarwal-shivam <60382983+agarwal-shivam@users.noreply.github.com>
2020-02-06 15:14:50 +0530

    Add files via upload

---
+++ product.js
@@ -0,0 +1,48 @@
+// var product1=
+// {
+//     "Name":"Knife";
+//     "Description":"Use to...";
+//     "Price":67;
+
+// }
+//phone
+var phone={
+"name":"OPPO A7 (Glaze Blue, 3GB RAM, 64GB Storage)",
+"product_description":"A7 adopts the industry-first water drop screen design, backed by several technological advances, reflecting nature like a water droplet on the verge of dropping. Corning glass supports an 88.4 percent screen ratio, providing resistance to scratches",
+"sold_by":"Appario Retail Private Ltd",
+"price":"231",
+"warranty_details":"1 year manufacturer warranty for device and 6 months manufacturer warranty for in-box accessories including batteries from the date of purchase"
+};
+
+var output1=document.getElementById("output1");
+output1.innerHTML="Name:"+phone.name;
+output1.innerHTML="Product Description"+phone.product_description;
+output1.innerHTML="Delivery by:"+phone.delivery_by;
+output1.innerHTML="Sold by:"+phone.sold_by;
+output1.innerHTML="Price"+phone.price;
+output1.innerHTML="Warranty details"+phone.warranty_details;
+
+
+
+
+
+
+
+
+
+//camera
+var camera={
+"name":"Canon EOS 1500D 24.1 Digital SLR Camera (Black) with EF S18-55 is II Lens, 16GB Card and Carry Case",
+"product_description":"All camera users, even beginners, will be able to capture amazing images and movies with this DSLR camera",
+"delivery_by":"Wed",
+"sold_by":"Appario Retail Private Ltd",
+"price":"231",
+"warranty_details":"1 year manufacturer warranty for device and 6 months manufacturer warranty for in-box accessories including batteries from the date of purchase"
+}
+var output2=document.getElementById('output2');
+output2.innerHTML="Name:"+camera.name;
+output2.innerHTML="Product Description:"+camera.product_description;
+output2.innerHTML="Delivery by:"+camera.delivery_by;
+output2.innerHTML="Sold by:"+camera.sold_by;
+output2.innerHTML="Price:"+camera.price;
+output2.innerHTML="Warranty details:"+camera.warranty_details;
