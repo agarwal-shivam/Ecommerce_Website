b1ac4decf48e83ca02e41e09c7ff1c2ac03bcc54
agarwal-shivam <60382983+agarwal-shivam@users.noreply.github.com>
2020-03-11 11:20:41 +0530

    HEROKU CHANGES

--- server.js
+++ server.js
@@ -68,4 +68,4 @@ app.get('*', function (req, res) {
 });
 
 //port listening
-app.listen(process.env.PORT, () => console.log('Listening on port ' + port));
+app.listen(process.env.PORT, () => console.log('Listening on port ' + process.env.PORT));
